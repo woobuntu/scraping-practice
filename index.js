910121a64b5505df9465cb5fcb4c8a61d477b76b
in uh, lee <boutime2017@gmail.com>
2021-08-19 18:27:26 +0900

    puppeteer로 페이지 열기

--- index.js
+++ index.js
@@ -1,44 +1,10 @@
-const axios = require("axios");
-const fs = require("fs");
-const cheerio = require("cheerio");
+const puppeteer = require("puppeteer");
 
 (async function main() {
   try {
-    const { data: html } = await axios({
-      method: "get",
-      url: "https://www.codingwithstefan.com/table-example/",
-    });
-
-    const $ = cheerio.load(html);
-
-    const rowsInTable = $("body > table > tbody > tr");
-
-    const tableHeaders = [];
-
-    const scrapedRows = rowsInTable
-      .map((index, tableRow) => {
-        if (index == 0) {
-          tableHeaders.push(
-            ...$(tableRow)
-              .find("th")
-              .map((i, th) => $(th).text())
-              .toArray()
-          );
-          return;
-        }
-
-        const tableDatas = $(tableRow)
-          .find("td")
-          .map((i, td) => $(td).text())
-          .toArray();
-
-        return tableHeaders.reduce(
-          (obj, th, index) => ((obj[th] = tableDatas[index]), obj),
-          {}
-        );
-      })
-      .toArray();
-    console.log(scrapedRows);
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.goto("https://google.com");
   } catch (error) {
     throw error;
   }
