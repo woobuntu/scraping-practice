3bfcd31b7fe6dcf6f2726d0900d402b46dfbdea3
in uh, lee <boutime2017@gmail.com>
2021-08-19 14:52:10 +0900

    table 동적 scraping

--- index.js
+++ index.js
@@ -6,13 +6,39 @@ const cheerio = require("cheerio");
   try {
     const { data: html } = await axios({
       method: "get",
-      url: "https://reactnativetutorial.net/css-selectors/lesson6.html",
+      url: "https://www.codingwithstefan.com/table-example/",
     });
-    fs.writeFileSync("./test.html", html);
 
     const $ = cheerio.load(html);
 
-    console.log($("[data-customer='22293']").text());
+    const rowsInTable = $("body > table > tbody > tr");
+
+    const tableHeaders = [];
+
+    const scrapedRows = rowsInTable
+      .map((index, tableRow) => {
+        if (index == 0) {
+          tableHeaders.push(
+            ...$(tableRow)
+              .find("th")
+              .map((i, th) => $(th).text())
+              .toArray()
+          );
+          return;
+        }
+
+        const tableDatas = $(tableRow)
+          .find("td")
+          .map((i, td) => $(td).text())
+          .toArray();
+
+        return tableHeaders.reduce(
+          (obj, th, index) => ((obj[th] = tableDatas[index]), obj),
+          {}
+        );
+      })
+      .toArray();
+    console.log(scrapedRows);
   } catch (error) {
     throw error;
   }
